17c8e26be47a767b50baf41ace76eb6839c1a37a
Oliver Wheeler <oliver.kuruma@gmail.com>
2020-06-25 00:27:51 +0200

    Move HomePage into Home Component

---
+++ src/Home/Home.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ConferenceList from "../ConferenceList/ConferenceList";
+
+export const Home: React.FC = () => (
+  <>
+    <h1>Cloud Native Conference Web App</h1>
+    <ConferenceList />
+  </>
+);
